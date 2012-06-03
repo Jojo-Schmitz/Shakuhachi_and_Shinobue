33ae5c00c0d39ea8cd133e9b26141be8332d6551
Joachim Schmitz <jojo@schmitz-digital.de>
2012-06-03 13:22:58 +0200

    first commit
    of tcbnhrs' version

---
+++ shakuhachi18.js
@@ -0,0 +1,91 @@
+//=============================================================================
+//  MuseScore
+//  Linux Music Score Editor
+//  $Id:$
+//
+//  Test plugin
+//
+//  Copyright (C)2008 Werner Schweer and others
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
+//
+// This is ECMAScript code (ECMA-262 aka "Java Script")
+//
+
+/*var fingerings = [ "z", "Z", "x", "X", "c", "v", "V", "b", "B", "n", "N", "m", 
+"a", "A", "s", "S", "d", "f", "F", "g", "G", "h", "H", "j",
+ "q", "Q", "w"] 
+*/
+var fingerings = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
+"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
+ "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+"m", "n", "o", "p", "q", "r"] 
+
+//---------------------------------------------------------
+//    init
+//    this function will be called on startup of mscore
+//---------------------------------------------------------
+
+function init()
+      {
+      // print("test script init");
+      }
+
+//-------------------------------------------------------------------
+//    run
+//    this function will be called when activating the
+//    plugin menu entry
+//
+//    global Variables:
+//    pluginPath - contains the plugin path; file separator is "/"
+//-------------------------------------------------------------------
+
+function run()
+      {
+      var cursor   = new Cursor(curScore);
+      cursor.staff = 0;
+      cursor.voice = 0;
+      cursor.rewind();  // set cursor to first chord/rest
+      var font = new QFont("Shakuhachi18", 15);
+      while (!cursor.eos()) {
+            if (cursor.isChord()) {
+                  
+                  var pitch = cursor.chord().topNote().pitch;
+                  var index = pitch-78+             18   //                            Shakuhachi Length [寸  (su-n)]
+                  if(index >= 0 && index < fingerings.length-5){ 
+                      var text  = new Text(curScore);
+                      text.text = fingerings[index];
+                      text.defaultFont = font;
+                      text.yOffset = 6;
+                      cursor.putStaffText(text);
+                      }
+                  }
+            cursor.next();
+            }
+      }
+
+//---------------------------------------------------------
+//    menu:  defines were the function will be placed
+//           in the MuseScore menu structure
+//---------------------------------------------------------
+
+var mscorePlugin = {
+      menu: 'Plugins.Shakuhachi18 Notes',
+      init: init,
+      run:  run
+      };
+
+mscorePlugin;
+
